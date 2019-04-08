2c89ed5d403e1de544f580f41fb24ac0ef7cfaac
Victor Zhang <vzhang@dm.ai>
2019-04-08 08:13:18 -0700

    flood fill

--- scripts/model.py
+++ scripts/model.py
@@ -75,25 +75,16 @@ class Model:
         shape = []
         for name, fluent in sorted(self._fluent_dict.items()):
             shape.append(len(fluent.range()))
-        self.tensor = np.zeros(shape) + np.nan
+        self.input_tensor = np.zeros(shape) + np.nan
         # print(np.shape(tensor))
-        print(self.tensor)
+        print(self.input_tensor)
         print("prefer fluents")
         for fluent_vec in self._favorable_fluent_vectors:
-            self.tensor[self.fluent_vec_to_index(fluent_vec)] = 1
+            self.input_tensor[self.fluent_vec_to_index(fluent_vec)] = 1
         print("avoid fluents")
         for fluent_vec in self._unfavorable_fluent_vectors:
-            self.tensor[self.fluent_vec_to_index(fluent_vec)] = 0
-        print(self.tensor)
-
-        self.X = []
-        self.Y = []
-        for fluent_vec in self._favorable_fluent_vectors:
-            self.X.append(self.fluent_vec_to_np_vec(fluent_vec))
-            self.Y.append(1)
-        for fluent_vec in self._unfavorable_fluent_vectors:
-            self.X.append(self.fluent_vec_to_np_vec(fluent_vec))
-            self.Y.append(0)
+            self.input_tensor[self.fluent_vec_to_index(fluent_vec)] = 0
+        print(self.input_tensor)
 
     def fluent_vec_to_index(self, fluent_vec):
         indices = []
@@ -123,8 +114,18 @@ class Model:
 
     def train(self):
         print("start training")
+        output_tensor = self.floodfill(self.input_tensor)
+        # output_tensor = self.input_tensor
+        X = []
+        y = []
+        for index in np.ndindex(output_tensor.shape):
+            val = output_tensor[index]
+            if not np.isnan(val):
+                X.append(np.array(index))
+                y.append(val)
+
         self._regression_model = linear_model.LinearRegression()
-        self._regression_model.fit(self.X, self.Y)
+        self._regression_model.fit(X, y)
 
         print(self._regression_model.coef_)
         print(self._regression_model.intercept_)
@@ -137,11 +138,45 @@ class Model:
         return self._regression_model.predict(np.reshape(x, (1, -1)))
 
     def test2(self):
-        ret = np.zeros_like(self.tensor)
-        for index in np.ndindex(self.tensor.shape):
+        ret = np.zeros_like(self.input_tensor)
+        for index in np.ndindex(self.input_tensor.shape):
             x = self.index_to_np_vec(index)
             ret[index] = self._regression_model.predict(np.reshape(x, (1, -1)))
         print(ret)
+
+    def floodfill(self, input_tensor):  # input tensor is not modified
+        iters = 10
+        output_tensor = np.copy(input_tensor)
+        shape = output_tensor.shape
+
+        for i in range(iters):
+            for index in np.ndindex(shape):
+                if np.isnan(self.input_tensor[index]):  # don't modify 0 and 1 in the original input tensor
+                    neighbors = []  # array of index, each index a tuple, 1 connectivity
+                    for j in range(len(index)):
+                        temp1 = index[:j] + (index[j]+1, ) + index[j+1:]
+                        if 0 <= temp1[j] < shape[j]:
+                            neighbors.append(temp1)
+                        temp2 = index[:j] + (index[j]-1, ) + index[j+1:]
+                        if 0 <= temp2[j] < shape[j]:
+                            neighbors.append(temp2)
+                    new_val = 0
+                    count = 0
+                    # average of (neighbor * 0.9), excluding nan neighbors
+                    for neighbor in neighbors:
+                        temp = output_tensor[neighbor]
+                        if not np.isnan(temp):
+                            count += 1
+                            new_val += 0.9 * temp
+                    if count:
+                        output_tensor[index] = new_val / count
+                    else:
+                        output_tensor[index] = np.nan
+
+            print("iteration ", i)
+            print(output_tensor)
+        return output_tensor
+
 
 
 if __name__ == '__main__':
